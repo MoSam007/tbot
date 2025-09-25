ac64afa26beeaba61c79216303bd4c5668ada15b
MoSam007 <samato.moma@gmail.com>
2025-09-26 01:15:21 +0300

    Added debug logging to config files

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,16 @@
+// src/config/db.ts
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  global.__prisma ??
+  new PrismaClient({
+    log: ["query", "error"], // optional: useful for debugging
+  });
+
+if (process.env.NODE_ENV !== "production") global.__prisma = prisma;
 
 export default prisma;
